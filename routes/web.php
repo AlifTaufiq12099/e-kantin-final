30b75a88740225be30b08cff537b54fa1598fade
Alif Taufiq <aliftaufiq1209@email.com>
2025-11-27 13:45:59 +0700

    Disable Google login

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/logout', [AuthController::class, 'logout']); // Tambahan untuk GET method
 
 // Google OAuth
-Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('google.redirect');
-Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('google.callback');
+// Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('google.redirect');
+// Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('google.callback');
 
 // Dashboard Pembeli (Protected)
 Route::get('/home', function () {
